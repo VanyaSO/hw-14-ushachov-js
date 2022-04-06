cb5fd751ac822e221f76c63ef6da08be05999f10
Ivan Ushachov <ushachovg324@gmail.com>
2022-04-06 17:22:56 +0300

    й

--- js/script.js
+++ js/script.js
@@ -1,12 +1,21 @@
 'use strict'
 
-const number = +prompt('Введите число');
+const number = +prompt('Ведите число');
+let isSimple = true;
 
-for (let i = 2; i < number; i++){
-    if(number % i === 0){
-        console.log(false)
-        break;
-    }else{
-        console.log(true)
+if(number === 1) {
+    console.log(false)
+} else if(number > 1) {
+
+
+    for (let i = 2; i < number; i++){
+        if(number % i === 0){
+            isSimple = false;
+            break;
+        }
     }
+    console.log(isSimple)
+
 }
+
+
